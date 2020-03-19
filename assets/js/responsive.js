c6fae164c564f21bdbaf7c26c7240002b2e373ce
worrzell <aditya11maharana@gmail.com>
2020-03-19 17:25:24 +0530

    changes

--- assets/js/responsive.js
+++ assets/js/responsive.js
@@ -1,9 +1,6 @@
-
-
-$(window).resize(function () {
-    if ($(window).width() < 991) {
-        $('.nav-tabs').removeClass('flex-column');
-        $('.nav-tabs').addClass('.justify-content-center')
-    }
+$(window).resize(function() {
+  if ($(window).width() < 991) {
+    $(".nav-tabs").removeClass("flex-column");
+    $(".nav-tabs").addClass(".justify-content-center");
+  }
 });
-
